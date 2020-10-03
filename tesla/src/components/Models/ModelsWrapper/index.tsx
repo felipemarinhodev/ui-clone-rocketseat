4c5419091582975ff52742ba28acf7ed3f4b8f7e
Felipe Marinho <felipmarinho@gmail.com>
2020-10-03 20:33:06 -0300

    Add ModelsContext

--- tesla/src/components/Models/ModelsWrapper/index.tsx
+++ tesla/src/components/Models/ModelsWrapper/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+
+import ModelsContext, { CarModel } from '../ModelsContext';
 
 import { Container } from './styles';
 
 const ModelsWrapper: React.FC = ({ children }) => {
+	const wrapperRef = useRef<HTMLDivElement>(null)
+	const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+
+	const registerModel = useCallback((model: CarModel) => {
+		setRegisteredModels(state => [...state, model]);
+	}, []);
+
+	const unregisterModel = useCallback((modelName: string) => {
+		setRegisteredModels(state => state.filter(model => model.modelName !== modelName))
+	}, []);
+
+	const getModelByName = useCallback((modelName: string) => {
+		return registeredModels.find(item => item.modelName === modelName) || null
+	}, [registeredModels]);
+
 	return (
-		<Container>{children}</Container>
+		<ModelsContext.Provider value={{
+			wrapperRef,
+			registeredModels,
+			registerModel,
+			unregisterModel,
+			getModelByName
+		}}>
+			<Container ref={wrapperRef}>{children}</Container>
+		</ModelsContext.Provider>
 		);
 }
 
