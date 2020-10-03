92e6d9f9cfe1a0564d62d0d12145dcfcd956ae6b
Felipe Marinho <felipmarinho@gmail.com>
2020-10-03 14:29:29 -0300

    create DefaultOverlayContent

---
+++ tesla/src/components/DefaultOverlayContent/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const DefaultOverlayContent: React.FC = () => {
+	return (
+		<Container>
+			<h1>DefaultOverlayContent</h1>
+		</Container>
+	);
+}
+
+export default DefaultOverlayContent;
