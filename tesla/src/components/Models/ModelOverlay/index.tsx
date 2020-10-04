64d033c9eb52251e256ec71c9e9f4708b0faf75b
Felipe Marinho <felipmarinho@gmail.com>
2020-10-04 09:20:15 -0300

    get dimensions when resize window

--- tesla/src/components/Models/ModelOverlay/index.tsx
+++ tesla/src/components/Models/ModelOverlay/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
+import { CarModel } from '../ModelsContext';
 import useWrapperScroll from '../useWrapperScroll';
 
 import { Container } from './styles';
 
-const ModelOverlay: React.FC = ({children}) => {
-  const { scrollY } = useWrapperScroll();
+interface Props {
+  model: CarModel
+}
 
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
+
+const ModelOverlay: React.FC<Props> = ({model, children}) => {
+  const { scrollY } = useWrapperScroll();
+  
+  const getSectionDimentions = useCallback(() => {
+    const dimentions = model.sectionRef.current
+    return {
+      offsetTop: dimentions?.offsetTop,
+      offsetHeight: dimentions?.offsetHeight
+    } as SectionDimensions
+  }, [model.sectionRef])
+
+  const [dimensions, setDimentions] = useState<SectionDimensions>(
+    getSectionDimentions()
+  )
+
+  useLayoutEffect(() => {
+    function onResize() {
+      const data = getSectionDimentions()
+      console.log(data);
+      window.requestAnimationFrame(() => setDimentions(data))
+    }
+
+    window.addEventListener('resize', onResize)
+    return () => window.removeEventListener('resize', onResize)
+  }, [getSectionDimentions])
+  
   return (
     <Container>
       {children}
